8dde5188db79511c1557369f4ef8716d05a6f77b
DarklyZ <daniil.gorbik@gmail.com>
2019-11-11 01:36:25 +0500

    -

---
+++ bot.py
@@ -0,0 +1,302 @@
+# Для vk api
+from vk import VK, types
+from vk.bot_framework import Dispatcher, NamedRule, BaseMiddleware
+from vk.utils import TaskManager
+from vk.keyboards import Keyboard, ButtonColor
+# Уровень в боте
+from lvls import LVL
+# Стандартные модули
+from random import choice
+from json import loads
+from os import getenv
+from re import *
+
+dp = Dispatcher(VK(getenv('TOKEN')), getenv('GROUP_ID'))
+lvl_class = LVL(dp.vk)
+
+@dp.middleware()
+class Regist(BaseMiddleware):
+	async def pre_process_event(self, event, data):
+		if event['type'] == 'message_new' and event['object']['message']['from_id'] != event['object']['message']['peer_id']:
+			data['lvl'] = lvl_class(event['object']['message']['peer_id'])
+			from_id = event['object']['message']['from_id']
+			data['lvl'].request_lvl(from_id)
+			if not data['lvl'].request and from_id > 0:
+				data['lvl'].add_user(from_id)
+		return data
+
+	async def post_process_event(self):
+		pass
+
+@dp.setup_rule
+class Commands(NamedRule):
+	key = 'commands'
+
+	def __init__(self, commands):
+		self.commands = commands
+
+	async def check(self, message, data):
+		msg = message.text.lower().split()
+		if msg and msg[0][1:] in self.commands: return True
+		else: return False
+
+@dp.setup_rule
+class Payload(NamedRule):
+	key = 'payload'
+
+	def __init__(self, payload):
+		self.payload = payload
+
+	async def check(self, message, data):
+		if self.payload and message.payload : return True
+		elif not self.payload and not message.payload : return True
+		else: return False
+
+@dp.setup_rule
+class PayloadCommands(NamedRule):
+	key = 'payload_commands'
+
+	def __init__(self, commands):
+		self.commands = commands
+
+	async def check(self, message, data):
+		if message.payload and loads(message.payload)['command'] in self.commands : return True
+		else: return False
+
+@dp.setup_rule
+class IsAdmin(NamedRule):
+	key = 'is_admin'
+
+	def __init__(self, admin):
+		self.admin = admin
+	
+	async def check(self, message, data):
+		try: chat = (await dp.vk.api_request('messages.getConversationsById', {'peer_ids': message.peer_id}))['items'][0]['chat_settings']
+		except: is_admin = False
+		else: is_admin = message.from_id == chat['owner_id'] or message.from_id in chat['admin_ids']
+		if self.admin and is_admin: return True
+		elif not self.admin and not is_admin: return True
+		else: return False
+
+@dp.message_handler(commands = ['help'], count_args = 0, in_chat = True)
+async def help(message, data):
+	await message.answer('''Команды:
+1) /MyLVL - мой уровень
+2) /TopLVL[ <от>:<до>] - топ 10 участников
+3) /LVL & <id>+ - посмотреть уровни
+4) /Tele <count> & <id> - передать свою exp другому
+5) /BAN[ <причина>] & <id>+ - типо бан
+6) /Ord <chr>+ - код в юникоде символов''')
+
+@dp.message_handler(regex = r'^f+$', in_chat = True)
+async def f_f(message, data):
+	rand = choice((457241326,457241327,457241328,457241331,457241332,457241333,457241334,457241338,457241339))
+	await message.answer('Пал боец\nСмертью храбрых', attachment = f'photo-{dp.group_id}_{rand}')
+
+@dp.message_handler(regex = r'^[^\?]*\?{3}$', in_chat = True)
+async def hm_(message, data):
+	await message.answer('', attachment = f'photo-{dp.group_id}_457241329')
+
+@dp.message_handler(regex = r'^[^\?]*\?{2}$', in_chat = True)
+async def h_(message, data):
+	await message.answer('', attachment = f'photo-{dp.group_id}_457241330')
+
+@dp.message_handler(commands = ['toplvl'], count_args = 0, in_chat = True)
+@dp.message_handler(commands = ['toplvl'], have_args = [lambda arg: arg.isdigit(), lambda arg: arg.isdigit()], in_chat = True)
+async def toplvl_send(message, data):
+	reg_default = ('1', '10')
+	reg = (int(data.get('args', reg_default)[0]), int(data.get('args', reg_default)[1]))
+	await data['lvl'].toplvl_size(*reg)
+	await dp.vk.api_request('messages.send', {'random_id' : 0, 'peer_id' : message.peer_id, 'message' : data['lvl'].toplvl, 'disable_mentions' : True})
+
+@dp.message_handler(commands = ['mylvl'], count_args = 0, in_chat = True)
+async def mylvl(message, data):
+	id = message.from_id
+	await data['lvl'].send(id)
+	await message.answer(data['lvl'][id])
+
+@dp.message_handler(commands = ['lvl'], count_args = 0, with_reply_message = True, in_chat = True)
+async def lvl(message, data):
+	if message.reply_message.from_id > 0:
+		id = message.reply_message.from_id
+		await data['lvl'].send(id)
+		await message.answer(data['lvl'][id])
+
+ban_key = Keyboard(one_time = None, inline = True)
+ban_key.add_text_button('Ясно-понятно', ButtonColor.POSITIVE, {'command' : 'ban'})
+ban_key = ban_key.get_keyboard()
+@dp.message_handler(commands = ['ban'], with_reply_message = True, in_chat = True)
+async def ban(message, data):
+	if message.reply_message.from_id > 0:
+		texts = message.text.split(maxsplit = 1)
+		id = message.reply_message.from_id
+		await data['lvl'].user(id)
+		await message.answer(f"Бан пользователя:\n{data['lvl'][id]}\nПричина: {texts[1] if len(texts) == 2 else 'не указана'}", keyboard = ban_key)
+
+@dp.message_handler(commands = ['echo'], count_args = 0, is_admin = True, in_chat = True)
+async def echo(message, data):
+	try: member_ids = [item['member_id'] for item in (await dp.vk.api_request('messages.getConversationMembers', {'peer_id' : message.peer_id}))['items'] if item['member_id'] > 0 and item['member_id'] != message.from_id]	
+	except: pass
+	else:
+		id = message.from_id
+		await data['lvl'].user(id)
+		await message.answer(f"*{data['lvl'][id]} заорал на всю беседу*\nУслышали это:\n" + ''.join(f'[id{id}|💬]' for id in member_ids))
+
+@dp.message_handler(commands = ['setsmile'], have_args = [lambda arg: len(arg) <= 4], is_admin = True, with_reply_message = True, in_chat = True)
+async def set_smile(message, data):
+	if message.reply_message.from_id > 0:
+		data['lvl'].setsmile(data['args'][0], message.reply_message.from_id)
+		await message.answer(f"{data['args'][0]} : установлен")
+
+@dp.message_handler(commands = ['delsmile'], count_args = 0, is_admin = True, with_reply_message = True, in_chat = True)
+async def del_smile(message, data):
+	if message.reply_message.from_id > 0:
+		data['lvl'].delsmile(message.reply_message.from_id)
+		await message.answer('Смайл удалён')
+
+@dp.message_handler(commands = ['exp'], have_args = [lambda arg: arg.isdigit() or arg[1:].isdigit()], is_admin = True, with_reply_message = True, in_chat = True)
+async def exp(message, data):
+	if message.reply_message.from_id > 0:
+		id = message.reply_message.from_id
+		data['lvl'].insert_lvl(int(data['args'][0]), id)
+		await data['lvl'].send(id)
+		await message.answer(f"{data['args'][0]}:\n{data['lvl'][id]}")
+
+@dp.message_handler(commands = ['exp'], have_args = [lambda arg: arg in '+-'], is_admin = True, with_reply_message = True, in_chat = True)
+async def expp(message, data):
+	if message.reply_message.from_id > 0:
+		id = message.reply_message.from_id
+		attach = str(atta(message.reply_message.text, message.reply_message.attachments))
+		data['lvl'].insert_lvl(int(data['args'][0] + attach), id)
+		await data['lvl'].send(id)
+		await message.answer(f"{data['args'][0]}{attach}:\n{data['lvl'][id]}")
+
+@dp.message_handler(commands = ['tele'], have_args = [lambda arg: arg.isdigit() or arg[1:].isdigit()], with_reply_message = True, in_chat = True)
+async def tele(message, data):
+	if message.reply_message.from_id > 0:
+		count = int(data['args'][0])
+		id1, id2 = message.from_id, message.reply_message.from_id
+		data['lvl'].remove_exp(count, id1)
+		if data['lvl'].remove:
+			data['lvl'].insert_lvl(count, id2)
+			await data['lvl'].send(id1, id2)
+			await message.answer(f"{count}:\n{data['lvl'][id2]}\n{-count}:\n{data['lvl'][id1]}")
+		else: await message.answer('Не хватает exp!')
+
+@dp.message_handler(commands = ['ord'], count_args = 0, with_reply_message = True, in_chat = True)
+async def ordo(message, data):
+	ord_list = [ord(text) for text in message.reply_message.text]
+	await message.answer(f'Не знаю зачем тебе, но получай: {ord_list}')
+
+@dp.message_handler(commands = ['info'], count_args = 0, with_reply_message = True, in_chat = True)
+async def info(message, data):
+	attach = atta(message.reply_message.text, message.reply_message.attachments)
+	await message.answer(f'Стоимость сообщения {attach}')
+
+@dp.message_handler(commands = ['hello'], count_args = 0, is_admin = True, in_chat = True)
+async def hello_help(message, data):
+	await message.answer('''Ключевые слова:
+{title} - заголовок
+{user} - пользователь
+{name} - имя бота
+''')
+
+@dp.message_handler(commands = ['sethello'], in_chat = True, is_admin = True)
+async def hello_plus(message, data):
+	text = message.text.split(maxsplit = 1)[1]
+	try: hello = text.format(title = 'title', user = 'user', name = 'name')
+	except Exception as e: await message.answer(f'Ошибка "{e}"')
+	else:
+		data['lvl'].add_text(text)
+		await message.answer('Приветствие полученно\n' + hello)
+
+
+@dp.message_handler(commands = ['delhello'], count_args = 0, is_admin = True, in_chat = True)
+async def hello_del(message, data):
+	data['lvl'].del_text()
+	await message.answer('Приветствие удалено')
+
+#-----------------------------------------------------------------
+
+@dp.message_handler(chat_action = types.message.Action.chat_invite_user)
+async def add_user(message, data):
+	id1, id2 = message.from_id, message.action.member_id
+	data['lvl'].hello_text()
+	if id1 > 0 and id2 > 0 and data['lvl'].hello:
+		await data['lvl'].user(id1,id2)
+		if id1 != id2:
+			title = f"* Welcome to the club, buddy. *\nВас призвал(а): {data['lvl'][id1]}"
+			bot_name = (await dp.vk.api_request('groups.getById', {'group_id' : dp.group_id}))[0]['name']
+			blank = data['lvl'].hello.format(title = title, user = data['lvl'][id2], name = bot_name)
+			photo = 457241337
+		else:
+			blank = f"Вернулся(ась) {data['lvl'][id1]}."
+			photo = 457241328
+		await message.answer(blank, attachment = f'photo-{dp.group_id}_{photo}')
+
+@dp.message_handler(chat_action = types.message.Action.chat_kick_user, is_admin = False)
+async def remove_user(message, data):
+	id2 = message.action.member_id
+	data['lvl'].hello_text()
+	if id2 > 0 and data['lvl'].hello:
+		await data['lvl'].user(id2)
+		await message.answer(f"{data['lvl'][id2]} стал(а) натуралом(.", attachment = f'photo-{dp.group_id}_457241328')
+
+@dp.message_handler(chat_action = types.message.Action.chat_kick_user, is_admin = True)
+async def remove_user_admin(message, data):
+	id2 = message.action.member_id
+	data['lvl'].hello_text()
+	if id2 > 0 and data['lvl'].hello:
+		await data['lvl'].user(id2)
+		await message.answer(f"{data['lvl'][id2]} заебал(а) админа.", attachment = f'photo-{dp.group_id}_457241336')
+
+@dp.message_handler(chat_action = types.message.Action.chat_invite_user_by_link)
+async def add_user_link(message, data):
+	id1 = message.from_id
+	data['lvl'].hello_text()
+	if id1 > 0 and data['lvl'].hello:
+		await data['lvl'].user(id1)
+		title = f"* Welcome to the club, buddy. *\n* Вы попали в ловушку *"
+		bot_name = (await dp.vk.api_request('groups.getById', {'group_id': dp.group_id}))[0]['name']
+		blank = data['lvl'].hello.format(title = title, user = data['lvl'][id1], name = bot_name)
+		await message.answer(blank, attachment = f'photo-{dp.group_id}_457241337')
+
+def atta(text,attachments):
+	count = sum(3 if len(chars) >= 6 else 1 for chars in findall(r'[a-zа-яё]{2,}',text,I))
+	for attachment in attachments:
+		if attachment.type == 'photo':
+			pixel = max(size.width * size.height for size in attachment.photo.sizes)
+			count += round(pixel / (1280 * 720 / 70)) if pixel < 1280 * 720 else 70
+		elif attachment.type == 'wall' or attachment.doc and attachment.doc.ext == 'gif':
+			count += 20
+		elif attachment.type == 'audio_message':
+			count += round(attachment.audio_message.duration) if attachment.audio_message.duration < 25 else 25
+		elif attachment.type == 'video':
+			count += round(attachment.video.duration / 1.5) if attachment.video.duration < 60 * 2 else 80
+		elif attachment.type == 'sticker':
+			count += 10
+		elif attachment.type == 'audio':		
+			count += round(attachment.audio.duration / 3) if attachment.audio.duration < 60 * 3 else 60
+	return count
+
+@dp.message_handler(payload = False, in_chat = True)
+async def pass_lvl(message, data):
+	if message.from_id > 0:
+		attach = atta(message.text, message.attachments)
+		data['lvl'].insert_lvl(attach, message.from_id)
+	if search(r'смерт|суицид|умереть|гибну|окно',message.text, I): await message.answer(f'Вы написали:\n"{message.text}".\nЯ расценила это за попытку суицида.\n[id532695720|#бля_Оля_живи!!!!!]')
+	if search(r'\b(?:мирарукурин|мира|рару|руку|кури|рин)\b', message.text, I):
+		await dp.vk.api_request('messages.send', {'random_id' : 0, 'peer_id' : message.peer_id, 'sticker_id' : 9805})
+		await message.answer(f'[id121852428|💬]Ожидайте бана…')
+
+task_manager = TaskManager(dp.vk.loop)
+
+@task_manager.add_task
+async def run():
+	dp.run_polling()
+
+async def on_startup():
+	print('Бот работает')
+
+if __name__ == "__main__":
+	task_manager.run(auto_reload = True, on_startup = on_startup)
---
+++ lvls.py
@@ -0,0 +1,84 @@
+# База данных
+from psycopg2 import connect
+from psycopg2.extras import DictCursor
+# Стандартные модули
+from os import getenv
+
+con = connect(getenv('DATABASE_URL'), sslmode='require')
+cur = con.cursor(cursor_factory = DictCursor)
+con.autocommit = True
+
+class LVL(dict):
+	def __init__(self, vk):
+		super().__init__()
+		self.vk = vk
+
+	def __call__(self, peer_id):
+		self.peer_id = peer_id
+		return self
+
+	def insert_lvl(self, exp_count, *ids):
+		cur.execute("UPDATE LVL SET EXP = EXP + %s WHERE USER_ID in %s and PEER_ID = %s",(exp_count, ids, self.peer_id))
+		cur.execute("SELECT USER_ID,LVL,EXP FROM LVL WHERE (EXP < 0 or EXP >= LVL * 2000) and PEER_ID = %s", (self.peer_id,))
+		for row in cur.fetchall():
+			while row['exp'] >= row['lvl'] * 2000:
+				row['exp'] -= row['lvl'] * 2000
+				row['lvl'] += 1
+			while row['exp'] < 0 and row['lvl'] > 0:
+				row['lvl'] -= 1
+				row['exp'] += row['lvl'] * 2000
+			if row['lvl'] == 0: cur.execute("DELETE FROM LVL WHERE USER_ID = %s and PEER_ID = %s",(row['user_id'], self.peer_id))
+			else: cur.execute("UPDATE LVL SET LVL = %s,EXP = %s WHERE USER_ID = %s and PEER_ID = %s", (row['lvl'], row['exp'], row['user_id'], self.peer_id))
+
+	def remove_exp(self, exp_count, id):
+		cur.execute("SELECT USER_ID FROM LVL WHERE USER_ID = %s and EXP >= %s and PEER_ID = %s",(id, exp_count, self.peer_id))
+		self.remove = bool(cur.fetchone())
+		if self.remove:
+			cur.execute("UPDATE LVL SET EXP = EXP - %s WHERE USER_ID = %s and PEER_ID = %s",(exp_count, id, self.peer_id))
+
+	async def user(self, *ids):
+		cur.execute("SELECT USER_ID, SMILE FROM LVL WHERE USER_ID in %s and SMILE IS NOT NULL and PEER_ID = %s", (ids, self.peer_id))
+		smile = {row['user_id'] : row['smile'] for row in cur.fetchall()}
+		cur.execute("SELECT USER_ID FROM LVL WHERE PEER_ID = %s ORDER BY LVL DESC, EXP DESC LIMIT 3", (self.peer_id,))
+		top = {row['user_id'] : smile for row, smile in zip(cur.fetchall(), ('🥇', '🥈', '🥉'))}
+		self.update({user['id'] : f"{top.get(user['id'], '')}{user['first_name']} {user['last_name'][:3]}{smile.get(user['id'], '')}" for user in await self.vk.api_request('users.get', {'user_ids' : str(ids)[1:-1]})})
+
+	async def send(self, *ids):
+		cur.execute("SELECT USER_ID,LVL,EXP FROM LVL WHERE USER_ID in %s and PEER_ID = %s", (ids, self.peer_id))
+		lvl = {row['user_id'] : f"{row['lvl']}Ⓛ|{row['exp']}/{row['lvl'] * 2000}Ⓔ" for row in cur.fetchall()}
+		await self.user(*ids)
+		self.update({id : f"{self[id]}:{lvl.get(id, 'lvl:error')}" for id in ids})
+
+	async def toplvl_size(self, x, y):
+		try: cur.execute("SELECT row_number() OVER (ORDER BY LVL DESC,EXP DESC),USER_ID,LVL,EXP FROM LVL WHERE PEER_ID = %s LIMIT %s OFFSET %s", (self.peer_id, y - x + 1, x - 1))
+		except: self.toplvl = f'Я не могу отобразить {x} - {y}'
+		else:
+			rows = cur.fetchall()
+			await self.user(*(row['user_id'] for row in rows))
+			self.toplvl = f"TOP {rows[0]['row_number']} - {rows[-1]['row_number']}\n" + '\n'.join(f"[id{row['user_id']}|{row['row_number']}]:{self[row['user_id']]}:{row['lvl']}Ⓛ|{row['exp']}Ⓔ" for row in rows)
+
+	def request_lvl(self, id):
+		cur.execute("SELECT USER_ID FROM LVL WHERE USER_ID = %s and PEER_ID = %s", (id, self.peer_id))
+		self.request = bool(cur.fetchone())
+
+	def add_user(self, id):
+		cur.execute("INSERT INTO LVL (USER_ID, PEER_ID) VALUES (%s, %s)", (id, self.peer_id))
+
+	def setsmile(self, char, *ids):
+		cur.execute("UPDATE LVL SET SMILE = %s WHERE USER_ID in %s and PEER_ID = %s", (char, ids, self.peer_id))
+
+	def delsmile(self, *ids):
+		cur.execute("UPDATE LVL SET SMILE = NULL WHERE USER_ID in %s and PEER_ID = %s", (ids, self.peer_id))
+
+	def add_text(self, text):
+		self.hello_text()
+		if self.hello: cur.execute("UPDATE HELLO SET TEXT = %s WHERE PEER_ID = %s", (text, self.peer_id))
+		else: cur.execute("INSERT INTO HELLO (PEER_ID, TEXT) VALUES (%s, %s)", (self.peer_id, text))
+
+	def del_text(self):
+		cur.execute("DELETE FROM HELLO WHERE PEER_ID = %s", (self.peer_id,))
+
+	def hello_text(self):
+		cur.execute("SELECT TEXT FROM HELLO WHERE PEER_ID = %s", (self.peer_id,))
+		row = cur.fetchone()
+		self.hello = row['text'] if row else None
