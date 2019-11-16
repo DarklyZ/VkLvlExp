c33b803742936de5f484b7e79309168c3c207758
DarklyZ <daniil.gorbik@gmail.com>
2019-11-16 22:08:15 +0500

    +

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ from vk.utils import TaskManager
 from vk.keyboards import Keyboard, ButtonColor
 from lvls import LVL
 from random import choice
-from json import loads
+from json import loads, dumps
 from os import getenv
 from extra import *
 
@@ -17,12 +17,13 @@ lvl_class = LVL(dp.vk)
 
 @dp.middleware()
 class Regist(BaseMiddleware):
-	async def pre_process_event(self, event, data):
-		data['event'] = event
-		from_id, peer_id = event['object']['message']['from_id'], event['object']['message']['peer_id']
-		if event['type'] == 'message_new' and from_id != peer_id:
-			data['lvl'] = lvl_class(peer_id)
-			if not data['lvl'].check_user(from_id) and from_id > 0: data['lvl'].add_user(from_id)
+	async def pre_process_event(self, *args):
+		event, data = (loads(dumps(arg), object_hook = udict) for arg in args)
+		from_id, peer_id = event.object.message.from_id, event.object.message.peer_id
+		if event.type == 'message_new' and from_id != peer_id:
+			data.lvl = lvl_class(peer_id)
+			if not data.lvl.check_user(from_id) and from_id > 0: data.lvl.add_user(from_id)
+		data.update(event)
 		return data
 
 	async def post_process_event(self):
@@ -104,20 +105,20 @@ async def h_(message, data):
 async def toplvl_send(message, data):
 	reg_default = ('1', '10')
 	reg = (int(data.get('args', reg_default)[0]), int(data.get('args', reg_default)[1]))
-	await dp.vk.api_request('messages.send', {'random_id' : 0, 'peer_id' : message.peer_id, 'message' : await data['lvl'].toplvl_size(*reg), 'disable_mentions' : True})
+	await dp.vk.api_request('messages.send', {'random_id' : 0, 'peer_id' : message.peer_id, 'message' : await data.lvl.toplvl_size(*reg), 'disable_mentions' : True})
 
 @dp.message_handler(commands = ['mylvl'], count_args = 0, in_chat = True)
 async def mylvl(message, data):
 	id = message.from_id
-	await data['lvl'].send(id)
-	await message.answer(data['lvl'][id])
+	await data.lvl.send(id)
+	await message.answer(data.lvl[id])
 
 @dp.message_handler(commands = ['lvl'], count_args = 0, with_reply_message = True, in_chat = True)
 async def lvl(message, data):
 	if message.reply_message.from_id > 0:
 		id = message.reply_message.from_id
-		await data['lvl'].send(id)
-		await message.answer(data['lvl'][id])
+		await data.lvl.send(id)
+		await message.answer(data.lvl[id])
 
 ban_key = Keyboard(one_time = None, inline = True)
 ban_key.add_text_button('Ясно-понятно', ButtonColor.POSITIVE, {'command' : 'ban'})
@@ -127,8 +128,8 @@ async def ban(message, data):
 	if message.reply_message.from_id > 0:
 		texts = message.text.split(maxsplit = 1)
 		id = message.reply_message.from_id
-		await data['lvl'].user(id)
-		await message.answer(f"Бан пользователя:\n{data['lvl'][id]}\nПричина: {texts[1] if len(texts) == 2 else 'не указана'}", keyboard = ban_key)
+		await data.lvl.user(id)
+		await message.answer(f"Бан пользователя:\n{data.lvl[id]}\nПричина: {texts[1] if len(texts) == 2 else 'не указана'}", keyboard = ban_key)
 
 @dp.message_handler(commands = ['echo'], count_args = 0, is_admin = True, in_chat = True)
 async def echo(message, data):
@@ -136,19 +137,19 @@ async def echo(message, data):
 	except: pass
 	else:
 		id = message.from_id
-		await data['lvl'].user(id)
-		await message.answer(f"*{data['lvl'][id]} заорал на всю беседу*\nУслышали это:\n" + ''.join(f'[id{id}|💬]' for id in member_ids))
+		await data.lvl.user(id)
+		await message.answer(f"*{data.lvl[id]} заорал на всю беседу*\nУслышали это:\n" + ''.join(f'[id{id}|💬]' for id in member_ids))
 
 @dp.message_handler(commands = ['setsmile'], have_args = [lambda arg: len(arg) <= 4], is_admin = True, with_reply_message = True, in_chat = True)
 async def set_smile(message, data):
 	if message.reply_message.from_id > 0:
-		data['lvl'].setsmile(message.reply_message.from_id, smile = data['args'][0])
-		await message.answer(f"{data['args'][0]} : установлен")
+		data.lvl.setsmile(message.reply_message.from_id, smile = data.args[0])
+		await message.answer(f"{data.args[0]} : установлен")
 
 @dp.message_handler(commands = ['delsmile'], count_args = 0, is_admin = True, with_reply_message = True, in_chat = True)
 async def del_smile(message, data):
 	if message.reply_message.from_id > 0:
-		data['lvl'].setsmile(message.reply_message.from_id)
+		data.lvl.setsmile(message.reply_message.from_id)
 		await message.answer('Смайл удалён')
 
 @dp.message_handler(commands = ['exp'], have_args = [isint], is_admin = True, with_reply_message = True, in_chat = True)
@@ -156,38 +157,52 @@ async def del_smile(message, data):
 async def exp(message, data):
 	if message.reply_message.from_id > 0:
 		id = message.reply_message.from_id
-		if len(data['args']) == 2:
-			lvl, exp = int(data['args'][0]), int(data['args'][1])
+		if len(data.args) == 2:
+			lvl, exp = int(data.args[0]), int(data.args[1])
 			blank = f"{lvl:+}Ⓛ|{exp:+}Ⓔ:\n"
-			data['lvl'].insert_lvl(id, lvl = lvl, exp = exp)
+			data.lvl.insert_lvl(id, lvl = lvl, exp = exp)
 		else:
-			exp = int(data['args'][0])
+			exp = int(data.args[0])
 			blank = f"{exp:+}Ⓔ:\n"
-			data['lvl'].insert_lvl(id, exp = exp)
-		await data['lvl'].send(id)
-		await message.answer(blank + data['lvl'][id])
+			data.lvl.insert_lvl(id, exp = exp)
+		await data.lvl.send(id)
+		await message.answer(blank + data.lvl[id])
 
 @dp.message_handler(commands = ['exp'], have_args = [lambda arg: arg in '+-'], is_admin = True, with_reply_message = True, in_chat = True)
 async def expp(message, data):
 	if message.reply_message.from_id > 0:
 		id = message.reply_message.from_id
-		exp =  int(f"{data['args'][0]}{atta(message.reply_message.text, message.reply_message.attachments, data['event']['object']['message']['reply_message']['attachments'])}")
-		data['lvl'].insert_lvl(id, exp = exp)
-		await data['lvl'].send(id)
-		await message.answer(f"{exp:+}Ⓔ:\n{data['lvl'][id]}")
+		exp =  int(f"{data.args[0]}{atta(message.reply_message.text, data.object.message.reply_message.attachments)}")
+		data.lvl.insert_lvl(id, exp = exp)
+		await data.lvl.send(id)
+		await message.answer(f"{exp:+}Ⓔ:\n{data.lvl[id]}")
 
 @dp.message_handler(commands = ['tele'], have_args = [ispos], with_reply_message = True, in_chat = True)
 async def tele(message, data):
 	if message.reply_message.from_id > 0:
-		exp = int(data['args'][0])
 		id1, id2 = message.from_id, message.reply_message.from_id
-		if data['lvl'].remove_exp(id1, exp = exp):
-			data['lvl'].insert_lvl(id2, exp = exp)
-			await data['lvl'].send(id1, id2)
-			blank = f"{exp:+}Ⓔ:\n{data['lvl'][id2]}\n{-exp:+}Ⓔ:\n{data['lvl'][id1]}"
+		exp = int(data.args[0])
+		if data.lvl.remove_exp(id1, exp = exp):
+			data.lvl.insert_lvl(id2, exp = exp)
+			await data.lvl.send(id1, id2)
+			blank = f"{exp:+}Ⓔ:\n{data.lvl[id2]}\n{-exp:+}Ⓔ:\n{data.lvl[id1]}"
 		else:
-			await data['lvl'].send(id1)
-			blank = f"Не хватает Ⓔ!\n{data['lvl'][id1]}"
+			await data.lvl.send(id1)
+			blank = f"Не хватает Ⓔ!\n{data.lvl[id1]}"
+		await message.answer(blank)
+
+@dp.message_handler(commands = ['tele'], have_args = [lambda arg: arg == '+'], with_reply_message = True, in_chat = True)
+async def telep(message, data):
+	if message.reply_message.from_id > 0:
+		id1, id2 = message.from_id, message.reply_message.from_id
+		exp =  atta(message.reply_message.text, data.object.message.reply_message.attachments)
+		if data.lvl.remove_exp(id1, exp = exp):
+			data.lvl.insert_lvl(id2, exp = exp)
+			await data.lvl.send(id1, id2)
+			blank = f"{exp:+}Ⓔ:\n{data.lvl[id2]}\n{-exp:+}Ⓔ:\n{data.lvl[id1]}"
+		else:
+			await data.lvl.send(id1)
+			blank = f"Не хватает Ⓔ!\n{data.lvl[id1]}"
 		await message.answer(blank)
 
 @dp.message_handler(commands = ['ord'], count_args = 0, with_reply_message = True, in_chat = True)
@@ -197,7 +212,7 @@ async def ordo(message, data):
 
 @dp.message_handler(commands = ['info'], count_args = 0, with_reply_message = True, in_chat = True)
 async def info(message, data):
-	exp = atta(message.reply_message.text, message.reply_message.attachments, data['event']['object']['message']['reply_message']['attachments'])
+	exp = atta(message.reply_message.text, data.object.message.reply_message.attachments)
 	await message.answer(f"Стоимость сообщения {exp:+}Ⓔ")
 
 @dp.message_handler(commands = ['hello'], count_args = 0, is_admin = True, in_chat = True)
@@ -214,64 +229,64 @@ async def hello_plus(message, data):
 	try: hello = text.format(title = 'title', user = 'user', name = 'name')
 	except Exception as e: await message.answer(f'Ошибка "{e}"')
 	else:
-		data['lvl'].add_text(text)
+		data.lvl.add_text(text)
 		await message.answer('Приветствие полученно\n' + hello)
 
 @dp.message_handler(commands = ['delhello'], count_args = 0, is_admin = True, in_chat = True)
 async def hello_del(message, data):
-	data['lvl'].del_text()
+	data.lvl.del_text()
 	await message.answer('Приветствие удалено')
 
 @dp.message_handler(chat_action = types.message.Action.chat_invite_user)
 async def add_user(message, data):
 	id1, id2 = message.from_id, message.action.member_id
-	text = data['lvl'].hello_text()
+	text = data.lvl.hello_text()
 	if id1 > 0 and id2 > 0 and text is not None:
-		await data['lvl'].user(id1,id2)
+		await data.lvl.user(id1,id2)
 		if id1 != id2:
-			title = f"* Welcome to the club, buddy. *\nВас призвал(а): {data['lvl'][id1]}"
+			title = f"* Welcome to the club, buddy. *\nВас призвал(а): {data.lvl[id1]}"
 			bot_name = (await dp.vk.api_request('groups.getById', {'group_id' : dp.group_id}))[0]['name']
-			blank = text.format(title = title, user = data['lvl'][id2], name = bot_name)
+			blank = text.format(title = title, user = data.lvl[id2], name = bot_name)
 			photo = 457241337
 		else:
-			blank = f"Вернулся(ась) {data['lvl'][id1]}."
+			blank = f"Вернулся(ась) {data.lvl[id1]}."
 			photo = 457241328
 		await message.answer(blank, attachment = f'photo-{dp.group_id}_{photo}')
 
 @dp.message_handler(chat_action = types.message.Action.chat_kick_user, is_admin = False)
 async def remove_user(message, data):
 	id2 = message.action.member_id
-	if id2 > 0 and data['lvl'].hello_text() is not None:
-		await data['lvl'].user(id2)
-		await message.answer(f"{data['lvl'][id2]} стал(а) натуралом(.", attachment = f'photo-{dp.group_id}_457241328')
+	if id2 > 0 and data.lvl.hello_text() is not None:
+		await data.lvl.user(id2)
+		await message.answer(f"{data.lvl[id2]} стал(а) натуралом(.", attachment = f'photo-{dp.group_id}_457241328')
 
 @dp.message_handler(chat_action = types.message.Action.chat_kick_user, is_admin = True)
 async def remove_user_admin(message, data):
 	id2 = message.action.member_id
-	if id2 > 0 and data['lvl'].hello_text() is not None:
-		await data['lvl'].user(id2)
-		await message.answer(f"{data['lvl'][id2]} заебал(а) админа.", attachment = f'photo-{dp.group_id}_457241336')
+	if id2 > 0 and data.lvl.hello_text() is not None:
+		await data.lvl.user(id2)
+		await message.answer(f"{data.lvl[id2]} заебал(а) админа.", attachment = f'photo-{dp.group_id}_457241336')
 
 @dp.message_handler(chat_action = types.message.Action.chat_invite_user_by_link)
 async def add_user_link(message, data):
 	id1 = message.from_id
-	text = data['lvl'].hello_text()
+	text = data.lvl.hello_text()
 	if id1 > 0 and text is not None:
-		await data['lvl'].user(id1)
+		await data.lvl.user(id1)
 		title = f"* Welcome to the club, buddy. *\n* Вы попали в ловушку *"
 		bot_name = (await dp.vk.api_request('groups.getById', {'group_id': dp.group_id}))[0]['name']
-		blank = text.format(title = title, user = data['lvl'][id1], name = bot_name)
+		blank = text.format(title = title, user = data.lvl[id1], name = bot_name)
 		await message.answer(blank, attachment = f'photo-{dp.group_id}_457241337')
 
 @dp.message_handler(with_payload = False, in_chat = True)
 async def pass_lvl(message, data):
 	if message.from_id > 0:
-		exp = atta(message.text, message.attachments, data['event']['object']['message']['attachments'])
-		data['lvl'].insert_lvl(message.from_id, exp = exp)
-	if search(r'смерт|суицид|умереть|гибну|окно',message.text, I): await message.answer(f"Вы написали:\n\"{message.text}\".\nЯ расценила это за попытку суицида.\n[id{data['lvl'].getconst('olga_id')}|#бля_Оля_живи!!!!!]")
+		exp = atta(message.text, data.object.message.attachments)
+		data.lvl.insert_lvl(message.from_id, exp = exp)
+	if search(r'смерт|суицид|умереть|гибну|окно',message.text, I): await message.answer(f"Вы написали:\n\"{message.text}\".\nЯ расценила это за попытку суицида.\n[id{data.lvl.getconst('olga_id')}|#бля_Оля_живи!!!!!]")
 	if search(r'\b(?:мирарукурин|мира|рару|руку|кури|рин)\b', message.text, I):
 		await dp.vk.api_request('messages.send', {'random_id' : 0, 'peer_id' : message.peer_id, 'sticker_id' : 9805})
-		await message.answer(f"[id{data['lvl'].getconst('archi_id')}|💬][id{message.from_id}|🃏]Ожидайте бана…")
+		await message.answer(f"[id{data.lvl.getconst('archi_id')}|💬][id{message.from_id}|🃏]Ожидайте бана…")
 
 @task_manager.add_task
 async def run():
--- extra.py
+++ extra.py
@@ -1,6 +1,9 @@
-from vk.types.wall_post import WallPost
-from itertools import zip_longest
 from re import *
+
+class udict(dict):
+    __getattr__ = dict.get
+    __setattr__ = dict.__setitem__
+    __delattr__ = dict.__delitem__
 
 def isint(arg):
 	try: int(arg)
@@ -11,16 +14,14 @@ def ispos(arg):
 	try: return int(arg) >= 0
 	except ValueError: return False
 
-def atta(text = '', attachments = [], event_attachments = []):
+def atta(text = '', attachments = []):
 	s = sum(3 if len(chars) >= 6 else 1 for chars in findall(r'\b[a-zа-яё]{3,}\b', text, I))
 	count = s if s < 50 else 50
-	for attachment, event_attachment in zip_longest(attachments, event_attachments):
+	for attachment in attachments:
 		if attachment.type == 'photo':
 			pixel = max(size.width * size.height for size in attachment.photo.sizes)
 			count += round(pixel / (1280 * 720 / 70)) if pixel < 1280 * 720 else 70
-		elif attachment.type == 'wall' and event_attachment is not None:
-			wall = WallPost(**event_attachment['wall'])
-			if wall.attachments is not None: count += atta(attachments = wall.attachments)
+		elif attachment.type == 'wall': count += atta(attachment.wall.text, attachment.wall.attachments)
 		elif attachment.type == 'doc' and attachment.doc.ext == 'gif': count += 20
 		elif attachment.type == 'audio_message': count += round(attachment.audio_message.duration) if attachment.audio_message.duration < 25 else 25
 		elif attachment.type == 'video': count += round(attachment.video.duration / 1.5) if attachment.video.duration < 60 * 2 else 80
