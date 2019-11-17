7f84d4a224d3a56da119af16a265ceab2172d75f
DarklyZ <daniil.gorbik@gmail.com>
2019-11-17 17:17:19 +0500

    +

--- bot.py
+++ bot.py
@@ -297,8 +297,8 @@ async def run():
 	dp.run_polling()
 
 async def on_shutdown():
+	dp.vk.close()
 	await lvl_class.close_db()
-	task_manager.close()
 
 if __name__ == "__main__":
 	task_manager.run(auto_reload = True, on_shutdown = on_shutdown)
