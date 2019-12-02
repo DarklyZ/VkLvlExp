8a6d2218eed8214988a14d60640fb3d97564a399
DarklyZ <daniil.gorbik@gmail.com>
2019-12-03 04:37:56 +0500

    +

--- lvls.py
+++ lvls.py
@@ -1,15 +1,22 @@
 from asyncpg import connect
-from datetime import date
+from datetime import tzinfo, datetime, timedelta
+
+class tz(tzinfo):
+	utcoffset = lambda self, dt : timedelta(hours = 5)
+	dst = lambda self, dt : timedelta()
+	tzname = lambda self, dt : '+05:00'
+tz = tz()
+
+bdate = lambda user, date : '🎂' if 'bdate' in user and user['bdate'].startswith(f"{date.day}.{date.month}") else ''
 
 class LVL(dict):
 	def __init__(self, vk):
 		super().__init__()
 		self.vk = vk
 
-	def __call__(self, peer_id, unixtime = None):
+	def __call__(self, peer_id):
 		self.clear()
 		self.peer_id = peer_id
-		self.date = unixtime and date.fromtimestamp(unixtime)
 		return self
 	
 	async def connect_db(self, database_url):
@@ -43,14 +50,12 @@ class LVL(dict):
 			return True
 		else: return False
 	
-	bdate = lambda self, user : '🎂' if self.date is not None and 'bdate' in user and user['bdate'].startswith(f"{self.date.day}.{self.date.month}") else ''
-	
 	async def user(self, *ids):
 		rows = await self.con.fetch("select user_id, smile from lvl where user_id = any($1) and smile is not null and peer_id = $2", ids, self.peer_id)
 		smile = {row['user_id'] : row['smile'] for row in rows}
 		rows = await self.con.fetch("select user_id from lvl where peer_id = $1 order by lvl desc, exp desc limit 3", self.peer_id)
 		top = {row['user_id'] : smile for row, smile in zip(rows, '🥇🥈🥉')}
-		self.update({user['id'] : f"{top.get(user['id'], '')}{self.bdate(user)}{user['first_name']} {user['last_name'][:3]}{smile.get(user['id'], '')}" for user in await self.vk.api_request('users.get', {'user_ids' : str(ids)[1:-1], 'fields' : 'bdate'})})
+		self.update({user['id'] : f"{top.get(user['id'], '')}{bdate(user, datetime.now(tz))}{user['first_name']} {user['last_name'][:3]}{smile.get(user['id'], '')}" for user in await self.vk.api_request('users.get', {'user_ids' : str(ids)[1:-1], 'fields' : 'bdate'})})
 
 	async def send(self, *ids):
 		rows = await self.con.fetch("select user_id,lvl,exp from lvl where user_id = any($1) and peer_id = $2", ids, self.peer_id)
