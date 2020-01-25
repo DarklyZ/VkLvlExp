8283f1ff80b80a08b559aa3ca417a17e5b72c0db
DarklyZ <daniil.gorbik@gmail.com>
2020-01-25 21:09:34 +0500

    +

--- chat_action_commands.py
+++ chat_action_commands.py
@@ -27,13 +27,13 @@ def load(bot, lvl_class):
 	
 	@bot.on.chat_action('chat_invite_user')
 	async def add_user(message):
-		id1, id2 = message.from_id, message.action.member_id
 		text = await lvl_class.hello_text()
 		if not text: return
+		id1, id2 = message.from_id, message.action.member_id
 		await lvl_class.user(id1,id2)
 		if id1 != id2:
 			title = f"* Welcome to the club, buddy. *\nВас призвал(а): {lvl_class[id1]}"
-			bot_name = (await bot.api.groups.getById(group_id = group_id))[0]['name']
+			bot_name = (await bot.api.groups.getById(group_id = bot.group_id))[0]['name']
 			blank = text.format(title = title, user = lvl_class[id2], name = bot_name)
 			photo = 457241337
 		else:
@@ -43,18 +43,18 @@ def load(bot, lvl_class):
 	
 	@bot.on.chat_action('chat_kick_user')
 	async def remove_user(message):
-		id2 = message.action.member_id
 		if not await lvl_class.hello_text(): return
+		id2 = message.action.member_id
 		await lvl_class.user(id2)
 		if await is_admin(message): await message(f"{lvl_class[id2]} заебал(а) админа.", attachment = f'photo-{bot.group_id}_457241336')
 		else: await message(f"{lvl_class[id2]} стал(а) натуралом(.", attachment = f'photo-{bot.group_id}_457241328')
 	
 	@bot.on.chat_action('chat_invite_user_by_link')
 	async def add_user_link(message):
-		id1 = message.from_id
 		text = await lvl_class.hello_text()
 		if not text: return
+		id1 = message.from_id
 		await lvl_class.user(id1)
 		title = f"* Welcome to the club, buddy. *\n* Вы попали в ловушку *"
-		bot_name = (await vk.api_request('groups.getById', {'group_id': group_id}))[0]['name']
+		bot_name = (await bot.api.groups.getById(group_id = bot.group_id))[0]['name']
 		await message(text.format(title = title, user = lvl_class[id1], name = bot_name), attachment = f'photo-{bot.group_id}_457241337')
