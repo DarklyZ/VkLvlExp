a2f362828d7f73acc23be2fa716a792c20fc96d5
Даниил <51062020+DarklyZ@users.noreply.github.com>
2020-02-26 22:49:12 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -25,6 +25,6 @@ async def pass_lvl(message):
 	lvl_class(message.peer_id)
 	if message.peer_id == message.from_id or message.from_id < 0: return
 	await lvl_class.check_add_user(message.from_id)
-	if not message.payload and (exp := atta(message.text, message.attachments, message.from_id)): await lvl_class.insert_lvl(message.from_id, exp = exp)
+	if not message.payload and (exp := atta(message.text, message.attachments)): await lvl_class.insert_lvl(message.from_id, exp = exp)
 
 bot.run_polling()
