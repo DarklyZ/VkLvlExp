be663d87e28bcae1343be3e3788cd19e6829eca6
NoName <daniil.gorbik@gmail.com>
2020-04-30 22:18:18 +0500

    +-

--- lvls.py
+++ lvls.py
@@ -78,7 +78,6 @@ class LVL(dict, ContextInstanceMixin):
 				for row in await self.con.fetch("select user_id, smile from lvl where user_id = any($1) and smile is not null and peer_id = $2", ids, self.peer_id)}
 		top = {row['user_id'] : smile
 				for row, smile in zip(await self.con.fetch("select user_id from lvl where peer_id = $1 order by lvl desc, exp desc limit 3", self.peer_id), '🥇🥈🥉')}
-		print(await self.api.users.get(user_ids = ids, fields = 'bdate'))
 		self.update({user.id : f"{top.get(user.id, '')}{bdate(user, now)}{user.first_name} {user.last_name[:3]}{smile.get(user.id, '')}"
 		        for user in await self.api.users.get(user_ids = ids, fields = 'bdate')})
 
