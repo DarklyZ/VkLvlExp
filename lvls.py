1e5046b51132830fdcafcd4c00abbfcfd2037d7d
Даниил <51062020+DarklyZ@users.noreply.github.com>
2020-05-22 21:10:41 +0500

    Update lvls.py

--- lvls.py
+++ lvls.py
@@ -21,8 +21,9 @@ class timezone(tzinfo):
 tz = timezone()
 
 def atta(text = '', attachments = [], negative = False, return_errors = False):
+	text = sub(r'https?://\S+', '', text) if text else None
 	if text:
-		text = sub(r'https?://', '', text)
+		text = 
 		dict_errors = {change['word'] : change['s'] for change in speller.spell(text)}
 		s = sum(3 if len(chars) >= 6 else 1 for chars in split(r'[^a-zа-яё]+', text, I) if len(chars) >= 3 and chars not in dict_errors)
 		count = s if s < 50 else 50
