c446e5196d7e2270c0204cb948820c668f0036b0
NoName <daniil.gorbik@gmail.com>
2020-06-11 18:02:42 +0500

    +-

--- audio_message.py
+++ audio_message.py
@@ -4,14 +4,16 @@ from vkbottle.http.request import HTTP
 from vkbottle.api import get_api
 from io import BytesIO
 
-class params(BaseModel):
-	voice = 'alena'
-	format = 'mp3'
-	quality = 'hi'
-	speed = 1.15
-	text: str
-
 class AMessage(HTTP, ContextInstanceMixin):
+	url = 'http://tts.voicetech.yandex.net/tts'
+
+	class params(BaseModel):
+		voice = 'alena'
+		format = 'mp3'
+		quality = 'hi'
+		speed = 1.15
+		text: str
+
 	def __init__(self):
 		self.api = get_api()
 		self.set_current(self)
@@ -20,7 +22,7 @@ class AMessage(HTTP, ContextInstanceMixin):
 		self.peer_id = peer_id
 
 	async def get_doc(self, text):
-		content = await self.request.get('http://tts.voicetech.yandex.net/tts', data = params(text = text).dict(), read_content = True)
+		content = await self.request.get(self.url, data = self.params(text = text).dict(), read_content = True)
 		server = await self.api.docs.get_messages_upload_server(type = 'audio_message', peer_id = self.peer_id)
 		with BytesIO(content) as f:
 			file = (await self.request.post(server.upload_url, data = {'file': f}))['file']
