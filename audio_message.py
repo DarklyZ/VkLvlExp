5f2c5d7340aac408342522c8a2c85b775db3fdcf
NoName <daniil.gorbik@gmail.com>
2020-06-13 13:34:35 +0500

    +-

--- audio_message.py
+++ audio_message.py
@@ -1,16 +1,16 @@
 from vkbottle.utils import ContextInstanceMixin
 from vkbottle.types.base import BaseModel
-from vkbottle.http.request import HTTP
+from aiohttp import request
 from init import InitParams
 from io import BytesIO
 
-class AMessage(HTTP, ContextInstanceMixin, InitParams):
+class AMessage(ContextInstanceMixin, InitParams):
 	url = 'http://tts.voicetech.yandex.net/tts'
 
 	class params(BaseModel):
 		voice = 'alyss'
 		emotion = 'evil'
-		speed = 1.2
+		speed = '1.2'
 		text: str
 
 	def __init__(self):
@@ -20,10 +20,11 @@ class AMessage(HTTP, ContextInstanceMixin, InitParams):
 		self.peer_id = peer_id
 
 	async def get_doc(self, text):
-		content = await self.request.get(self.url, data = self.params(text = text).dict(), read_content = True)
+		async with request('GET', self.url, params = self.params(text = text).dict()) as response:
+			bfile = await response.content.read()
 		server = await self.api.docs.get_messages_upload_server(type = 'audio_message', peer_id = self.peer_id)
-		with BytesIO(content) as f:
-			file = (await self.request.post(server.upload_url, data = {'file': f}))['file']
+		async with request('POST', server.upload_url, data = {'file': BytesIO(bfile)}) as response:
+			file = (await response.json())['file']
 		save = await self.api.docs.save(file = file)
 		return f'doc{save.audio_message.owner_id}_{save.audio_message.id}'
 
