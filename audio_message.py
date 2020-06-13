f00be164b08b61474788cbf4ec3ea79025fcda6f
NoName <daniil.gorbik@gmail.com>
2020-06-13 19:39:24 +0500

    +-

--- audio_message.py
+++ audio_message.py
@@ -20,12 +20,11 @@ class AMessage(ContextInstanceMixin, InitParams):
 		self.peer_id = peer_id
 
 	async def get_doc(self, text):
-		async with request('GET', self.url, params = self.params(text = text).dict()) as response:
-			bfile = await response.content.read()
 		server = await self.api.docs.get_messages_upload_server(type = 'audio_message', peer_id = self.peer_id)
-		async with request('POST', server.upload_url, data = {'file': BytesIO(bfile)}) as response:
-			file = (await response.json())['file']
-		save = await self.api.docs.save(file = file)
+		async with request('GET', self.url, params = self.params(text = text).dict()) as response:
+			with BytesIO(await response.content.read()) as bfile:
+				async with request('POST', server.upload_url, data = {'file': bfile}) as response:
+					save = await self.api.docs.save(file = (await response.json())['file'])
 		return f'doc{save.audio_message.owner_id}_{save.audio_message.id}'
 
 	async def get_text(self, audio_message):
