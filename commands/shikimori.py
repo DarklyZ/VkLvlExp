b8bdca7a3ce5f12275f9ed7af3de142f64273564
NoName <daniil.gorbik@gmail.com>
2020-06-30 21:21:52 +0500

    +-

--- commands/shikimori.py
+++ commands/shikimori.py
@@ -4,7 +4,7 @@ class ShikimoriCommands(InitParams):
 	def load(self):
 		@self.bot.on.chat_message(text = ['ss <type:inc[animes,mangas,ranobe,characters]> <page:pos> <text>',
 				'ss <type:inc[animes,mangas,ranobe,characters]> <text>'], command = True)
-		async def _(message, type, text, page = 1):
+		async def shiki_search(message, type, text, page = 1):
 			response = await self.shiki.search(text)
 			docs = await self.shiki.get_doc([item['image']['original'] for item in response], page)
-			await message('Не найдено' if not docs else None, attachment = ','.join(docs))
+			await message('Не найдено' if not docs else '\n'.join(item['russian'] for item in response), attachment = ','.join(docs))
