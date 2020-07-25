35089744d9f93a552b20e2e3ce16b2792e73cfa3
NoName <daniil.gorbik@gmail.com>
2020-07-25 21:04:15 +0500

    +-

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ class Validators(PatchedValidators):
 	inc = lambda self, value, *extra: value.lower() if value.lower() in extra else None
 
 Patcher(validators = Validators, flags = I + S)
-bot = Bot(getenv('TOKEN'), debug = True)
+bot = Bot(getenv('TOKEN'), debug = False)
 
 task = TaskManager(bot.loop)
 task.add_task(bot.run(True))
