352216145b53664de7391e58fe76498b80256f57
NoName <daniil.gorbik@gmail.com>
2020-07-28 18:54:47 +0500

    +-

--- utils/api.py
+++ utils/api.py
@@ -13,9 +13,10 @@ class ShikiApi(InitParams.Params):
 	async def search(self, type, text, page, limit = 5):
 		params, types = {'search': text}, ['chatacters', 'people']
 		if type not in types: params.update({'censored': 'false', 'page': page, 'limit': str(limit)})
-		async with request('GET', self.url_shiki_api.format(method = type + ('/search' if type in types else '')), params = params) as response:
+		else: type += '/search'
+		async with request('GET', self.url_shiki_api.format(method = type), params = params) as response:
 			res = await response.json()
-			return res[(page - 1) * limit : (page - 1) * limit + limit] if type in types else res
+			return res[(page - 1) * limit : (page - 1) * limit + limit] if len(params) == 1 else res
 
 	async def get_shiki_short_link(self, url):
 		return (await self.bot.api.utils.get_short_link(self.url_shiki.format(url = url))).short_url[8:]
@@ -37,17 +38,14 @@ class ShikiApi(InitParams.Params):
 
 class AMessage(InitParams.Params):
 	url = 'http://tts.voicetech.yandex.net/tts'
-
-	class Params(dict):
-		val = {'voice': 'alyss', 'emotion': 'evil', 'speed': '1.1'}
-		__init__ = lambda self, **var: super().__init__(**self.val, **var)
+	params = {'voice': 'alyss', 'emotion': 'evil', 'speed': '1.1'}
 
 	def __call__(self, peer_id):
 		self.peer_id = peer_id
 
 	async def get_doc(self, text):
 		server = await self.bot.api.docs.get_messages_upload_server(type = 'audio_message', peer_id = self.peer_id)
-		async with request('GET', self.url, params = self.Params(text = text)) as response:
+		async with request('GET', self.url, params = {'text': text, **self.params}) as response:
 			with BytesIO(await response.read()) as bfile:
 				async with request('POST', server.upload_url, data = {'file': bfile}) as response:
 					save = await self.bot.api.docs.save(file = (await response.json())['file'])
