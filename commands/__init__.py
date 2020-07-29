bf6f5ba185f836194d5e36e265dd09f28cf3c3f5
NoName <daniil.gorbik@gmail.com>
2020-07-29 22:53:40 +0500

    +-

--- commands/__init__.py
+++ commands/__init__.py
@@ -10,18 +10,16 @@ class HelpCommand(InitData.Data):
 	help = [
 		'/Help LVL - уровни',
 		'/Help Nick - ники',
-		'/Help Extra - доп. команды',
-		'/Help Shiki - шикимори'
+		'/Help Extra - доп. команды'
 	]
 
 	dict_help = {
 		'lvl': LVLCommands,
 		'nick': NickCommands,
 		'extra': ExtraCommands,
-		'shiki': ShikimoriCommands
 	}
 
 	def __init__(self):
-		@self.bot.on.chat_message(text = ['help', 'help <extra:inc[lvl,nick,extra,shiki]>'], command = True)
+		@self.bot.on.chat_message(text = ['help', 'help <extra:inc[lvl,nick,extra]>'], command = True)
 		async def help(message, extra = None):
 			await message(f"Команды{f' {extra.title()}' if extra else ''}:\n" + '\n'.join(f'{n + 1}) {comm}' for n, comm in enumerate(self.dict_help.get(extra, self).help)))
