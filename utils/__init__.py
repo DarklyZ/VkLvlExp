1763b22c8457177859d9f7f189a0bdc3a4889bb4
NoName <daniil.gorbik@gmail.com>
2020-07-29 22:36:41 +0500

    +-

--- utils/__init__.py
+++ utils/__init__.py
@@ -5,7 +5,7 @@ class InitData:
 	def __init__(self, token, database_url, debug = False):
 		from utils.lvls import LVL
 		from vkbottle import Bot
-		from utils.api import ShikiApi, ThisWaifuDoesNotExist, AMessage, Foaf
+		from utils.api import ShikiApi, ThisWaifuDoesNotExist, AMessage, FoafPHP
 
 		self.bot = Bot(token, debug = debug)
 		self.lvl_class = LVL(database_url)
