945eeaae76db841c53a60466d22bb664031f8620
NoName <daniil.gorbik@gmail.com>
2020-11-24 12:43:34 +0500

    +-

--- override_vkbottle_types/objects.py
+++ override_vkbottle_types/objects.py
@@ -1,8 +1,26 @@
 from vkbottle_types.objects import *
+from .set_attr import SetAttr
 
-class ChatSettings(BaseModel):
+@SetAttr(MessagesConversation, 'chat_settings')
+class ChatSettings(BaseObject):
     owner_id: int
     admin_ids: list
 
-class MessagesConversation(MessagesConversation):
-    chat_settings: Optional[ChatSettings] = None
+@SetAttr(MessagesMessageAttachment, 'type')
+class MessagesMessageAttachmentType(enum.Enum):
+    PHOTO = "photo"
+    AUDIO = "audio"
+    VIDEO = "video"
+    DOC = "doc"
+    POLL = "poll"
+    LINK = "link"
+    MARKET = "market"
+    MARKET_ALBUM = "market_album"
+    GIFT = "gift"
+    STICKER = "sticker"
+    STORY = "story"
+    WALL = "wall"
+    WALL_REPLY = "wall_reply"
+    ARTICLE = "article"
+    GRAFFITI = "graffiti"
+    AUDIO_MESSAGE = "audio_message"
---
+++ override_vkbottle_types/set_attr.py
@@ -0,0 +1,16 @@
+from pydantic.fields import ModelField
+
+class SetAttr:
+    def __init__(self, cls, name):
+        self.cls = cls
+        self.name = name
+
+    def __call__(self, type):
+        self.set(self.cls, self.name, type)
+
+    @staticmethod
+    def set(cls, name, type, class_validators = None, default = None, required = False):
+        cls.__fields__[name] = ModelField(
+            name = name, type_ = type, class_validators = class_validators,
+            model_config = cls.Config, default = default, required = required
+        )
