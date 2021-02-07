d2cf0d2b69b4898b545c85498922e1b09102f391
NoName <daniil.gorbik@gmail.com>
2021-02-07 20:55:58 +0500

    refactoring

--- webutils/__init__.py
+++ webutils/__init__.py
@@ -3,18 +3,17 @@ from json.decoder import JSONDecodeError
 from .routes import routes
 from os import getenv
 
-def in_routes(handler):
+def is_custom(handler):
 	return handler.__module__ == 'webutils.routes'
 
 @middleware
 async def middleware(request, handler):
 	if (request.headers.getone('TOKEN', None) != getenv('TOKEN')):
-		return json_response({'error': "Invalid token"})
+		return json_response({'error': "Invalid token"}, status = 403)
 	elif (request.content_type != 'application/json'):
-		return json_response({'error': "Content type must be 'application/json'"})
-	else:
-		try: return await handler(**await request.json()) if in_routes(handler) else await handler(request)
-		except (TypeError, JSONDecodeError) as e: return json_response({'error': str(e)})
+		return json_response({'error': "Content type must be 'application/json'"}, status = 400)
+	try: return await handler(**await request.json()) if is_custom(handler) else await handler(request)
+	except (TypeError, JSONDecodeError) as e: return json_response({'error': str(e)}, status = 400)
 
 app = Application(middlewares = [middleware])
 app.add_routes(routes)
