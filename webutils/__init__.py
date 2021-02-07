5701f1e5df20a57a185e8e9c42bd181b8fe36e41
NoName <daniil.gorbik@gmail.com>
2021-02-07 21:31:40 +0500

    refactoring

--- webutils/__init__.py
+++ webutils/__init__.py
@@ -1,19 +1,15 @@
-from aiohttp.web import Application, json_response, middleware, _run_app
-from json.decoder import JSONDecodeError
+from aiohttp.web import Application, Response, middleware, _run_app
 from .routes import routes
 from os import getenv
-
-def is_custom(handler):
-	return handler.__module__ == 'webutils.routes'
 
 @middleware
 async def middleware(request, handler):
 	if (request.headers.getone('TOKEN', None) != getenv('TOKEN')):
-		return json_response({'error': "Invalid token"}, status = 403)
+		return Response(text = "Invalid token", status = 403)
 	elif (request.content_type != 'application/json'):
-		return json_response({'error': "Content type must be 'application/json'"}, status = 400)
-	try: return await handler(**await request.json()) if is_custom(handler) else await handler(request)
-	except (TypeError, JSONDecodeError) as e: return json_response({'error': str(e)}, status = 400)
+		return Response(text = "Content type must be 'application/json'", status = 400)
+	else:
+		return await handler(request)
 
 app = Application(middlewares = [middleware])
 app.add_routes(routes)
--- webutils/routes.py
+++ webutils/routes.py
@@ -1,14 +1,25 @@
 from utils import Data as data
-from aiohttp.web import json_response, RouteTableDef
+from aiohttp.web import Response, json_response, RouteTableDef
+from json.decoder import JSONDecodeError
 
 routes = RouteTableDef()
 
+def json_load(coro):
+	async def decorator(request):
+		try:
+			return await coro(**await request.json())
+		except (TypeError, JSONDecodeError) as e:
+			return Response(text = str(e), status = 400)
+	return decorator
+
 @routes.post('/get_lvl')
+@json_load
 async def get_lvl(peer_id, user_ids):
 	await data.lvlweb(peer_id).get_lvl(*user_ids)
 	return json_response(data.lvlweb)
 
 @routes.post('/get_top')
+@json_load
 async def get_top(peer_id, x, y):
 	await data.lvlweb(peer_id).get_top(x, y)
 	return json_response(data.lvlweb)
