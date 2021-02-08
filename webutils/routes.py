09449ff8bf48c9a2111dd246d8a599c9c46a25e7
NoName <daniil.gorbik@gmail.com>
2021-02-08 17:22:19 +0500

    refactoring

--- webutils/routes.py
+++ webutils/routes.py
@@ -15,13 +15,13 @@ async def bot(request):
 	return Response(text = 'ok')
 
 @routes.post('/get_lvl')
-@options(json = True, token = True, content_type = 'application/json')
+@options(json = True, secret_key = True, content_type = 'application/json')
 async def get_lvl(peer_id, user_ids):
 	await data.lvl(peer_id).get_lvl(*user_ids)
 	return json_response(data.lvl)
 
 @routes.post('/get_top')
-@options(json = True, token = True, content_type = 'application/json')
+@options(json = True, secret_key = True, content_type = 'application/json')
 async def get_top(peer_id, x, y):
 	await data.lvl(peer_id).get_top(x, y)
 	return json_response(data.lvl)
