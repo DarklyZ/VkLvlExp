54187a24852aea754340fffc681f305492e6d505
darklyz <daniil.gorbik@gmail.com>
2021-04-10 20:00:40 +0500

    fix

--- utils/lvl.py
+++ utils/lvl.py
@@ -65,7 +65,7 @@ class LVL(dict, Data):
 
 	#ALL
 
-	async def update_lvl(self, *ids, lvl = 0, exp = 0, boost = False, temp = False, slave = False, check = True):
+	async def update_lvl(self, *ids, lvl = 0, exp = 0, boost = False, temp = False, slave = False):
 		await self.con.execute("update lvl set lvl = lvl + $1, exp = exp + $2, temp_exp = temp_exp + $3 where user_id = any($4) and peer_id = $5", lvl, exp, exp if temp else 0, ids, self.peer_id)
 
 		if boost:
@@ -79,19 +79,18 @@ class LVL(dict, Data):
 			masters = {row['master']: row['slcount']
 				for row in await self.con.fetch("select master, slcount from lvl where user_id = any($1) and work is not null and peer_id = $2", ids, self.peer_id)}
 			for key, group in groupby(masters, lambda id: masters[id]):
-				await self.update_lvl(group, exp = exp * getpercent(key) / 100)
+				await self.update_lvl(*group, exp = exp * getpercent(key) / 100)
 
-		if check:
-			for row in await self.con.fetch("select user_id, lvl, exp from lvl where (exp < 0 or lvl < 1 or exp >= lvl * 2000) and peer_id = $1", self.peer_id):
-				row_lvl, row_exp = row['lvl'], row['exp']
-				while row_exp >= row_lvl * 2000:
-					row_exp -= row_lvl * 2000
-					row_lvl += 1
-				while row_exp < 0 and row_lvl > 0:
-					row_lvl -= 1
-					row_exp += row_lvl * 2000
-				if row_lvl > 0: await self.con.execute("update lvl set lvl = $1, exp = $2, master = null, work = null where user_id = $3 and peer_id = $4", row_lvl, row_exp, row['user_id'], self.peer_id)
-				else: await self.con.execute("delete from lvl where user_id = $1 and peer_id = $2", row['user_id'], self.peer_id)
+		for row in await self.con.fetch("select user_id, lvl, exp from lvl where (exp < 0 or lvl < 1 or exp >= lvl * 2000) and peer_id = $1", self.peer_id):
+			row_lvl, row_exp = row['lvl'], row['exp']
+			while row_exp >= row_lvl * 2000:
+				row_exp -= row_lvl * 2000
+				row_lvl += 1
+			while row_exp < 0 and row_lvl > 0:
+				row_lvl -= 1
+				row_exp += row_lvl * 2000
+			if row_lvl > 0: await self.con.execute("update lvl set lvl = $1, exp = $2, master = null, work = null where user_id = $3 and peer_id = $4", row_lvl, row_exp, row['user_id'], self.peer_id)
+			else: await self.con.execute("delete from lvl where user_id = $1 and peer_id = $2", row['user_id'], self.peer_id)
 
 	async def remove_exp(self, id, exp = 0):
 		if allow := (await self.con.fetchrow("select count(user_id) > 0 as bool from lvl where user_id = $1 and exp >= $2 and peer_id = $3", id, exp, self.peer_id))['bool']:
