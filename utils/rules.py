77beed6ec2e350fa6eca8be4fb978e55256735c0
DarklyZ <daniil.gorbik@gmail.com>
2022-01-27 16:13:30 +0500

    test

--- utils/rules.py
+++ utils/rules.py
@@ -14,25 +14,25 @@ class SetRule:
 		custom_rules[self.name] = cls
 		return cls
 
+def get_patcher():
+	patcher = Patcher()
+	@patcher.validator()
+	def int(value):
+		return int(value) if value.isdigit() or value[:1] in '+-' and value[1:].isdigit() else None
+	@patcher.validator()
+	def pos(value):
+		return int(value) if value.isdigit() or value[:1] == '+' and value[1:].isdigit() else None
+	@patcher.validator()
+	def max(value, extra):
+		return value if len(value) <= int(extra) else None
+	@patcher.validator()
+	def inc(value, *extra):
+		return value.lower() if value.lower() in extra else None
+	return patcher
+
 @SetRule('command')
 class CommandVBMLRule(VBMLRule):
-	patcher = Patcher()
-
-	@patcher.validator(key = 'int')
-	def int_validator(value):
-		return int(value) if value.isdigit() or value[:1] in '+-' and value[1:].isdigit() else None
-
-	@patcher.validator(key = 'pos')
-	def pos_validator(value):
-		return int(value) if value.isdigit() or value[:1] == '+' and value[1:].isdigit() else None
-
-	@patcher.validator(key = 'max')
-	def max_validator(value, extra):
-		return value if len(value) <= int(extra) else None
-
-	@patcher.validator(key = 'inc')
-	def inc_validator(value, *extra):
-		return value.lower() if value.lower() in extra else None
+	patcher = get_patcher()
 
 	def __init__(self, pattern):
 		regex = r'[\./!:]{}$'
