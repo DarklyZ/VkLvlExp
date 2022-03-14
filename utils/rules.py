51f32781f009b8cbbf65fbd6be72d2b8099f8f14
DarklyZ <daniil.gorbik@gmail.com>
2022-03-14 21:46:10 +0500

    refactoring

--- utils/rules.py
+++ utils/rules.py
@@ -30,25 +30,22 @@ class CommandVBMLRule(VBMLRule):
 
 		self.patterns = pattern
 
-	@staticmethod
-	@patcher.validator(key = 'int')
-	def int_validator(value):
-		return int(value) if value.isdigit() or value[:1] in '+-' and value[1:].isdigit() else None
-
-	@staticmethod
-	@patcher.validator(key = 'pos')
-	def pos_validator(value):
-		return int(value) if value.isdigit() or value[:1] == '+' and value[1:].isdigit() else None
-
-	@staticmethod
-	@patcher.validator(key = 'max')
-	def max_validator(value, extra):
-		return value if len(value) <= int(extra) else None
-
-	@staticmethod
-	@patcher.validator(key = 'inc')
-	def inc_validator(value, *extra):
-		return value.lower() if value.lower() in extra else None
+	@property
+	def patcher(self):
+		patcher = type(self).patcher = Patcher()
+		@patcher.validator(key = 'int')
+		def int_validator(value):
+			return int(value) if value.isdigit() or value[:1] in '+-' and value[1:].isdigit() else None
+		@patcher.validator(key = 'pos')
+		def pos_validator(value):
+			return int(value) if value.isdigit() or value[:1] == '+' and value[1:].isdigit() else None
+		@patcher.validator(key = 'max')
+		def max_validator(value, extra):
+			return value if len(value) <= int(extra) else None
+		@patcher.validator(key = 'inc')
+		def inc_validator(value, *extra):
+			return value.lower() if value.lower() in extra else None
+		return patcher
 
 @SetRule('audio_message')
 class AudioMessage(VBMLRule, Data):
