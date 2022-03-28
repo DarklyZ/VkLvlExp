8826861d34167d606df340ad4cae26e09aec98fd
DarklyZ <daniil.gorbik@gmail.com>
2022-03-28 15:22:37 +0500

    refactoring

--- webutils/__init__.py
+++ webutils/__init__.py
@@ -15,7 +15,7 @@ class Options(Data):
 					try: return await coro(request, **kwargs)
 					except TypeError: return Response(text = 'TypeError!', status = 500)
 					finally:
-						if 'user_id' in kwargs: self.lvl.clear()
+						if 'user_id' in kwargs: self.lvl(None)
 				else: return Response(text = 'Error!', status = 400)
 			except: return Response(text = 'Error!', status = 400)
 		return new_coro
