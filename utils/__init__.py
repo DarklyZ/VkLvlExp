203a7979b7b7568687b3ed3b10c6cb66e9c602e4
DarklyZ <daniil.gorbik@gmail.com>
2022-03-28 13:17:21 +0500

    refactoring

--- utils/__init__.py
+++ utils/__init__.py
@@ -2,5 +2,5 @@ class Data:
 	def __init_subclass__(cls, init = False):
 		if init:
 			for k, v in cls.__dict__.items():
-				if not k.startswith('_'): setattr(Data, k, v)
+				if not k[0] == '_': setattr(Data, k, v)
 			cls()
