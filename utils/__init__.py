e5d54755607c6fc771dfbf93c150f055814a4aba
DarklyZ <daniil.gorbik@gmail.com>
2022-04-04 12:06:15 +0500

    refactoring

--- utils/__init__.py
+++ utils/__init__.py
@@ -4,11 +4,5 @@ class Data:
 	def __init_subclass__(cls, write = False):
 		if write:
 			for k, v in cls.__dict__.items():
-				if not k[0] == '_': setattr(Data, k, v)
-
-	@abstractmethod
-	def __run__(self):
-		pass
-
-	def run(self):
-		return self.__run__()
+				if k == '__run__': setattr(cls, 'run', v)
+				elif not k[0] == '_': setattr(Data, k, v)
